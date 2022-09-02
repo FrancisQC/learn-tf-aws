2fbe9d523bb8d207ddc6ee7aecc532df46c23178
FrankQC <10887707+FrancisQC@users.noreply.github.com>
2022-09-02 22:59:50 +1000

    Adds variables and a module https://learn.hashicorp.com/tutorials/terraform/variables?in=terraform/certification-associate-tutorials#list-public-and-private-subnets

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-# Lets learn !!!
+# Let's learn !!!
--- variables.tf
+++ variables.tf
@@ -4,3 +4,28 @@ variable "aws_access_key_id" {
 variable "aws_secret_access_key" {
   type = string
 }
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_count" {
+  description = "Number of instances to provision."
+  type        = number
+  default     = 1
+}
+
+variable "enable_vpn_gateway" {
+  description = "Enable a VPN gateway in your VPC."
+  type        = bool
+  default     = false
+}
+
